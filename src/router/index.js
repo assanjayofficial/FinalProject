e1fcfac40d68d8606c2d7da1d97e51092791e3cf
assanjayofficial <s11820011@student.unklab.ac.id>
2021-05-05 17:02:52 +0700

    menambahkan FinalProject

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {SplashScreen, AppScreen} from '../pages';
+
+const Stack = createStackNavigator();
+const Router = () =>{
+    return(
+        <Stack.Navigator>
+            <Stack.Screen name="SplashScreen" component={SplashScreen} options={{headerShown: false}}/>
+            <Stack.Screen name="AppScreen" component={AppScreen} options={{headerShown: false}}/>
+        </Stack.Navigator>
+    );
+};
+
+export default Router;
